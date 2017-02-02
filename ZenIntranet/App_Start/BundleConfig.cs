a07b45fd2a648df28a2e5a5543c4310fc4de058f
zenits <thierry.sperissen@gmail.com>
2017-02-03 00:16:30 +0100

    add style scss

--- ZenIntranet/App_Start/BundleConfig.cs
+++ ZenIntranet/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace ZenIntranet
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/main.css"
+                      ));
         }
     }
 }
